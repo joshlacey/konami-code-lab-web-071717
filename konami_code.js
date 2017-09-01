bb34ec7daf8a066cb84d6d49eec80645ad57b584
joshlacey <williamjoshualacey@gmail.com>
2017-09-01 10:01:39 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0;
 
+function init() {
+  window.addEventListener('keydown', coding)
+}
+
+function coding(e) {
+  const key = parseInt(e.detail || e.which);
+  if (key === code[index]) {
+    console.log(key)
+    index++;
+    console.log(">>> "+index)
+    if (index === code.length) {
+      alert("You Did It");
+      index = 0;
+      console.log(">>> "+index)
+    }
+  } else {
+    console.log(key)
+    index = 0;
+    console.log(">>> "+index)
+  }
 }
